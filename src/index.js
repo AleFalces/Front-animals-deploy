31d379f27a7f5ccd0fa7c979ed453dbe464dd440
AleFalces <alefalces@gmail.com>
2025-05-05 15:13:26 -0300

    deploy changes

--- src/index.js
+++ src/index.js
@@ -14,26 +14,27 @@ import axios from "axios";
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientID = process.env.REACT_APP_AUTH0_CLIENT_ID;
 axios.defaults.baseURL = "https://backanimals-production.up.railway.app";
-//axios.defaults.baseURL = "http://localhost3001/";
+// axios.defaults.baseURL = "http://localhost:3001/";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	// <React.StrictMode>
-	<Auth0Provider
-		domain={domain}
-		clientId={clientID}
-		authorizationParams={{
-			redirect_uri: "https://buddyong.vercel.app/createAuth0",
-		}}>
-		<Provider store={store}>
-			<BrowserRouter>
-				<ChakraProvider theme={theme}>
-					<App />
-				</ChakraProvider>
-			</BrowserRouter>
-		</Provider>
-	</Auth0Provider>
-	// </React.StrictMode>
+  // <React.StrictMode>
+  <Auth0Provider
+    domain={domain}
+    clientId={clientID}
+    authorizationParams={{
+      redirect_uri: "https://buddyong.vercel.app/createAuth0",
+    }}
+  >
+    <Provider store={store}>
+      <BrowserRouter>
+        <ChakraProvider theme={theme}>
+          <App />
+        </ChakraProvider>
+      </BrowserRouter>
+    </Provider>
+  </Auth0Provider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
